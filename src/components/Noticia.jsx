844d80174aee1d73eea396cbcae9706c0339016e
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-04 00:20:50 -0300

    Le di estilo a las cards

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -4,7 +4,7 @@ import imgDefault from "../assets/img-noticias-default.jpg"
 const Noticia = ({ noticia }) => {
     return (
         <a href={noticia.link} className="enlace">
-            <Card className="card h-100">
+            <Card className="card h-100 rounded rounded-4">
                 <Card.Img className="img-card" variant="top" src={noticia.image_url ? noticia.image_url : imgDefault} alt="imagen de la Noticia" />
                 <Card.Body className="h-100">
                     <Card.Text>{noticia.creator ? noticia.creator : "Autor anónimo"}</Card.Text>
